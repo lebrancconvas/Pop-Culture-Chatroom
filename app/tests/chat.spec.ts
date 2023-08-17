64a2383e9d8f18380aaf1f3216f8682d5530a439
lebrancconvas <lebrancconvas@gmail.com>
2023-08-17 19:50:00 +0700

    ✅ test: test send message and show

--- app/tests/chat.spec.ts
+++ app/tests/chat.spec.ts
@@ -8,3 +8,14 @@ test('Show Starter Bot Message show Hello', async({ page }) => {
   await expect(page.locator('.bot-bubble p')).toHaveText('Hello!');
   await expect(page.locator('.user-bubble')).not.toBeVisible();
 });
+
+test('Send Message and Show User Message', async({ page }) => {
+  await page.goto('/');
+  await page.waitForLoadState('domcontentloaded');
+  await page.waitForTimeout(1000);
+
+  await page.locator('input').fill('Test!');
+  await page.keyboard.press('Enter');
+
+  await expect(page.locator('.user-bubble p')).toHaveText('Test!');
+});
