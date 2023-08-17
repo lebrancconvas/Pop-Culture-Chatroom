6c94e49a12e2ef2144498c5e67e34b537f085662
lebrancconvas <lebrancconvas@gmail.com>
2023-08-18 01:00:02 +0700

    🎉 feat(init): init scraper

---
+++ scraping/src/index.ts
@@ -0,0 +1,47 @@
+import { chromium } from "playwright";
+import { writeFile } from "fs/promises";
+import path from 'path';
+
+interface IManga {
+  title: string;
+  url: string;
+}
+
+let mangas: IManga[] = [];
+
+const index = async() => {
+
+  const browser = await chromium.launch({ headless: true });
+  const context = await browser.newContext({
+    locale: 'en-US'
+  });
+  const page = await context.newPage();
+
+  for(let i = 1; i < 1581; i++) {
+    const url = `https://mangakakalot.com/manga_list?type=latest&category=all&state=all&page=${i}`;
+    await page.goto(url);
+    await page.waitForSelector('.list-truyen-item-wrap > h3', { timeout: 100 });
+
+    const mangaList = await page.locator('.list-truyen-item-wrap > h3').all();
+
+    for(const manga of mangaList) {
+      const title = await manga.innerText();
+      const link = await manga.locator('a').getAttribute('href');
+
+      const mangaData: IManga = {
+        title: title || '',
+        url: link || ''
+      }
+
+      mangas.push(mangaData);
+      console.log('Save Log!');
+    }
+    await writeFile(path.join(__dirname, '..', '..', 'app', 'src', 'data', 'manga-data.json'), JSON.stringify(mangas, null, 2), 'utf-8');
+    console.log(`Page ${i} success!`);
+  }
+
+  await page.close();
+  await browser.close();
+};
+
+index();
